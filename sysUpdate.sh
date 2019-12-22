bc4e7dfe668f4b3ba2f438a065590107c60e4ea8
Dhruvik Mevada <dhruvik333@gmail.com>
2019-12-22 20:49:50 +0530

    [ADD] Linux Machine Update and Upgrade script

--- sysUpdate.sh
+++ sysUpdate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Type the following command to run the file  ./sysUpdate.sh
+# This is not much of a cool script but by running this script you can perform all commands wich saves time.
+# Happy Updating!!
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove
